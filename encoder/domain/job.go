1c61fcef61c06890bd4aae677c3e9f57a28bd236
Yuri Dias <yuri.dias@vaivoa.com>
2021-10-13 17:40:47 -0300

    feat: adicionando validação no modelo de job

--- encoder/domain/job.go
+++ encoder/domain/job.go
@@ -1,15 +1,54 @@
 package domain
 
-import "time"
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
 
-type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
 }
 
+type Job struct {
+	ID               string    `valid:"uuid"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull"`
+	Video            *Video    `valid:"-"`
+	VideoID          string    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"-"`
+	UpdatedAt        time.Time `valid:"-"`
+}
 
+func NewJob(output string, status string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: output,
+		Status:           status,
+		Video:            video,
+	}
+
+	job.prepare()
+
+	err := job.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &job, nil
+}
+
+func (j *Job) prepare() {
+	j.ID = uuid.NewV4().String()
+	j.CreatedAt = time.Now()
+	j.UpdatedAt = time.Now()
+}
+
+func (j *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(j)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
