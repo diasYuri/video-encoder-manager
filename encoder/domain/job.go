a670ca2376daecbd0b5916faca785704633698d3
Yuri Dias <yuri.dias@vaivoa.com>
2021-10-13 17:30:02 -0300

    feat: criando modelo de job

---
+++ encoder/domain/job.go
@@ -0,0 +1,15 @@
+package domain
+
+import "time"
+
+type Job struct {
+	ID               string
+	OutputBucketPath string
+	Status           string
+	Video            *Video
+	Error            string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+
