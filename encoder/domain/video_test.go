cfdab9b54ba4b168ed3e9de93a7f9c110d11a99d
Yuri Dias <yuri.dias@vaivoa.com>
2021-10-13 17:29:41 -0300

    test: testando validações do modelo de video

---
+++ encoder/domain/video_test.go
@@ -0,0 +1,38 @@
+package domain_test
+
+import (
+	"github.com/diasYuri/encoder-go/domain"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T){
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+	require.Empty(t, video)
+}
+
+func TestVideoIdIsNotAUuid(t *testing.T){
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+
+	err := video.Validate()
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T){
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.CreatedAt = time.Now()
+	video.ResourceID = "a"
+	video.Filepath = "path"
+
+	err := video.Validate()
+	require.Nil(t, err)
+}
