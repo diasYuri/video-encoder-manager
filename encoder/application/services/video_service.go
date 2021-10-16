3ebe0e48ec0e0cdded4f50cf1bdfcd857cdc74ad
Yuri Dias <yuri.dias@vaivoa.com>
2021-10-16 00:19:39 -0300

    feat: criando método de download e fragmentação em video service

---
+++ encoder/application/services/video_service.go
@@ -0,0 +1,88 @@
+package services
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"github.com/diasYuri/encoder-go/application/repositories"
+	"github.com/diasYuri/encoder-go/domain"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+type VideoService struct {
+	Video *domain.Video
+	VideoRepository repositories.VideoRepository
+}
+
+func NewVideoService() *VideoService{
+	return &VideoService{}
+}
+
+func (v *VideoService) Download(bucketName string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	bkt := client.Bucket(bucketName)
+	obj := bkt.Object(v.Video.FilePath)
+
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer r.Close()
+
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	_, err = f.Write(body)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("video %v has benn stored", v.Video.ID)
+
+	return nil
+}
+
+func (v *VideoService) Fragment() error  {
+
+	err := os.Mkdir(os.Getenv("localStoragePath")+"/"+v.Video.ID, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".frag"
+
+	cmd := exec.Command("mp4fragment", source, target)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+}
+
+
+func printOutput(out []byte){
+	if len(out) > 0{
+		log.Printf("=====> Output: %s\n", string(out))
+	}
+}
